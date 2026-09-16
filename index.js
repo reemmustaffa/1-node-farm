1f46af4dc672ec1fb408ce85638029d8a0841f8a
reem_mustafa <reemmustafa905@gmail.com>
2026-09-17 01:27:12 +0300

    Reading and Writing Files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const fs=require('fs');
+const fs = require("fs");
 
-const hello = "hello woorld";
-console.log(hello);
+//Read file
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+//Write file
+const textout = `This is what we know about the avocado ${textIn} created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textout);
+console.log("File Written!");
