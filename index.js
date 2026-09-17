8c15b9527ff0b15988f0a38611a74d99bed26b44
reem_mustafa <reemmustafa905@gmail.com>
2026-09-17 18:14:09 +0300

    Reading and Writing files Asynchronously

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+//Blocking == Syncr
 //Read file
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
@@ -8,3 +9,21 @@ console.log(textIn);
 const textout = `This is what we know about the avocado ${textIn} created on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textout);
 console.log("File Written!");
+
+//NonBlocking == Asyncr
+
+fs.readFile("./txt/startttttt.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!🔥");
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, (err) => {
+        console.log("Your file has been written☺️");
+      });
+    });
+  });
+});
+
+console.log("Will read file!");
